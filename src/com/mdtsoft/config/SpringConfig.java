23ebc6b1fa74ef83a692e9416dce5fa329d8dac5
tutkuince <tutku.ince@outlook.com>
2018-07-25 18:47:54 +0300

    remove redundant implementation

--- src/com/mdtsoft/config/SpringConfig.java
+++ src/com/mdtsoft/config/SpringConfig.java
@@ -5,13 +5,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.mdtsoft")
-public class SpringConfig implements WebMvcConfigurer{
+public class SpringConfig {
 	
 	@Bean
 	public ViewResolver viewResolver() {
