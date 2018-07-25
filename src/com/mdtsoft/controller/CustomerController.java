2889b4a221e9e3735828ce87d4fe7ce1e27160f5
tutkuince <tutku.ince@outlook.com>
2018-07-25 21:06:03 +0300

    injecting CustomerDAO and add customerList as a attribute to model

--- src/com/mdtsoft/controller/CustomerController.java
+++ src/com/mdtsoft/controller/CustomerController.java
@@ -1,16 +1,31 @@
 package com.mdtsoft.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mdtsoft.dao.CustomerDAO;
+import com.mdtsoft.model.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model model) {
 		
+		// get customers from the dao
+		List<Customer> customerList = customerDAO.getAll();
+		
+		//  add the customers to the model
+		model.addAttribute("customerList", customerList);
+
 		return "list-customers";
 	}
 }
