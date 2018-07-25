0dbfb4314c315a0712a46d50a04a2ef7f354b36c
tutkuince <tutku.ince@outlook.com>
2018-07-25 17:05:48 +0300

    Created WebServletConfiguration class for configure WebServlet.

---
+++ src/com/mdtsoft/config/WebServletConfiguration.java
@@ -0,0 +1,24 @@
+package com.mdtsoft.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebServletConfiguration implements WebApplicationInitializer{
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(SpringConfig.class);
+		context.setServletContext(servletContext);
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet());
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+		context.close();
+	}
+
+}
