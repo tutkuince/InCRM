c85227d28d20913452b7611c799285a60147d1c8
tutkuince <tutku.ince@outlook.com>
2018-07-27 13:28:38 +0300

    delete redundant import for @Transactional annotation

--- src/com/mdtsoft/dao/CustomerDAOImpl.java
+++ src/com/mdtsoft/dao/CustomerDAOImpl.java
@@ -7,7 +7,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.mdtsoft.model.Customer;
 
