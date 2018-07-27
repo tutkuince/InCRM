0f38ebca160b54e6ea7f37d10dc7c170c9a7c011
tutkuince <tutku.ince@outlook.com>
2018-07-27 13:30:49 +0300

    add unimplemented methods and @Transactional annotation

--- src/com/mdtsoft/service/CustomerServiceImpl.java
+++ src/com/mdtsoft/service/CustomerServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mdtsoft.dao.CustomerDAO;
 import com.mdtsoft.model.Customer;
@@ -19,6 +20,36 @@ public class CustomerServiceImpl implements CustomerService {
 		customers = customerDAO.getAll();
 		customers.sort((Customer c1, Customer c2) -> c1.getFirstName().compareTo(c2.getFirstName()));
 		return customers;
+	}
+
+	@Override
+	public void insert(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Customer getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@Transactional
+	public List<Customer> getAll() {
+		return customerDAO.getAll();
+	}
+
+	@Override
+	public void update(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
