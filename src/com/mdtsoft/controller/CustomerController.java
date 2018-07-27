c34805eb2b885eae2703857f67e16948cdbcf41c
tutkuince <tutku.ince@outlook.com>
2018-07-27 14:08:47 +0300

    added showFormForAdd() method for showing Save Customer page

--- src/com/mdtsoft/controller/CustomerController.java
+++ src/com/mdtsoft/controller/CustomerController.java
@@ -30,4 +30,14 @@ public class CustomerController {
 
 		return "list-customers";
 	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		
+		Customer customer = new Customer();
+		
+		model.addAttribute("customer", customer);
+		
+		return "customer-form";
+	}
 }
