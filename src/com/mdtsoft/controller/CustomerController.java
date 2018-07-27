12d1c6e2749f88a9cdb352341aab8e062f707090
tutkuince <tutku.ince@outlook.com>
2018-07-27 14:14:29 +0300

    added new feature about saving customer

--- src/com/mdtsoft/controller/CustomerController.java
+++ src/com/mdtsoft/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mdtsoft.model.Customer;
@@ -14,30 +16,36 @@ import com.mdtsoft.service.CustomerService;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
+
 	@Autowired
 	private CustomerService customerService;
-	
+
 //	@RequestMapping("/list")
 	@GetMapping("/list")
 	public String listCustomers(Model model) {
-		
-		// get customers from the dao
+
+		// get customers from the CustomerService
 		List<Customer> customerList = customerService.getAll();
-		
-		//  add the customers to the model
+
+		// add the customers to the model
 		model.addAttribute("customerList", customerList);
 
 		return "list-customers";
 	}
-	
+
 	@GetMapping("/showFormForAdd")
 	public String showFormForAdd(Model model) {
-		
+
 		Customer customer = new Customer();
-		
+
 		model.addAttribute("customer", customer);
-		
+
 		return "customer-form";
+	}
+
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+		customerService.insert(customer);
+		return "redirect:/customer/list";
 	}
 }
