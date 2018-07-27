d0d00faa0f7f17d7675ac32b4df0b6788fab569d
tutkuince <tutku.ince@outlook.com>
2018-07-27 13:21:10 +0300

    refactoring with @GetMapping annotation

--- src/com/mdtsoft/controller/CustomerController.java
+++ src/com/mdtsoft/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mdtsoft.dao.CustomerDAO;
@@ -17,7 +18,8 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")
+//	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		
 		// get customers from the dao
