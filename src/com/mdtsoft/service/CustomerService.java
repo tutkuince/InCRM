807d4b4b857cd543fc5944e5a4b5ef7496bab095
tutkuince <tutku.ince@outlook.com>
2018-07-27 13:30:13 +0300

    added new methods

--- src/com/mdtsoft/service/CustomerService.java
+++ src/com/mdtsoft/service/CustomerService.java
@@ -5,5 +5,10 @@ import java.util.List;
 import com.mdtsoft.model.Customer;
 
 public interface CustomerService {
+	void insert(Customer customer);
+	Customer getById(int id);
+	List<Customer> getAll();
+	void update(int id);
+	void delete(int id);
 	List<Customer> sortByName(List<Customer> customers);
 }
