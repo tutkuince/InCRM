649c6bbbec7ec56e754e4a384ff59a0f901b6857
tutkuince <tutku.ince@outlook.com>
2018-08-03 20:22:50 +0300

    added before advice and declarations

--- src/com/mdtsoft/aspect/CRMLoggingAspect.java
+++ src/com/mdtsoft/aspect/CRMLoggingAspect.java
@@ -2,7 +2,9 @@ package com.mdtsoft.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -13,6 +15,10 @@ public class CRMLoggingAspect {
 	private Logger logger = Logger.getLogger(getClass().getName());
 	
 	// add @Before advice
+	@Before("com.mdtsoft.aspect.AOPExpressions.forAppFlow()")
+	public void before(JoinPoint joinPoint) {
+		
+	}
 	
 	// add @AfterReturning advice
 }
