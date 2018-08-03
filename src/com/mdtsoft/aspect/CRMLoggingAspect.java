95d755936627447b954e181b41fb0cac96f56964
tutkuince <tutku.ince@outlook.com>
2018-08-03 20:34:10 +0300

    get the arguments

--- src/com/mdtsoft/aspect/CRMLoggingAspect.java
+++ src/com/mdtsoft/aspect/CRMLoggingAspect.java
@@ -1,5 +1,6 @@
 package com.mdtsoft.aspect;
 
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
@@ -22,7 +23,13 @@ public class CRMLoggingAspect {
 		String method = joinPoint.getSignature().toShortString();
 		logger.info("===>>> in @Before: calling method: " + method);
 		
+		
 		// display the arguments to the method
+		
+		// get the arguments
+		Object[] args = joinPoint.getArgs();
+		
+		Arrays.asList(args).stream().forEach(o -> logger.info("======>>>>>> argument: " + o));
 	}
 	
 	// add @AfterReturning advice
