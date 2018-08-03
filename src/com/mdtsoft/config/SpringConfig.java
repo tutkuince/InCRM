c4cd600667d9e588af8bd05b572021c1fae02a0a
tutkuince <tutku.ince@outlook.com>
2018-08-03 20:00:29 +0300

    added @EnableAspectJAutoProxy(proxyTargetClass = true)

--- src/com/mdtsoft/config/SpringConfig.java
+++ src/com/mdtsoft/config/SpringConfig.java
@@ -3,6 +3,7 @@ package com.mdtsoft.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.mdtsoft")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class SpringConfig implements WebMvcConfigurer {
 
 	@Bean
@@ -27,7 +29,7 @@ public class SpringConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
-	
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("index");
