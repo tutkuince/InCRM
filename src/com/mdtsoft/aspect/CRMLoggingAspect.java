95e6d089f6782f878bbe864a51dce04344ab5b6d
tutkuince <tutku.ince@outlook.com>
2018-08-03 20:39:59 +0300

    added @AfterReturning

--- src/com/mdtsoft/aspect/CRMLoggingAspect.java
+++ src/com/mdtsoft/aspect/CRMLoggingAspect.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,26 +12,35 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class CRMLoggingAspect {
-	
+
 	// setup logger
 	private Logger logger = Logger.getLogger(getClass().getName());
-	
+
 	// add @Before advice
 	@Before("com.mdtsoft.aspect.AOPExpressions.forAppFlow()")
 	public void before(JoinPoint joinPoint) {
-		
+
 		// display method we are calling
 		String method = joinPoint.getSignature().toShortString();
 		logger.info("===>>> in @Before: calling method: " + method);
-		
-		
+
 		// display the arguments to the method
-		
+
 		// get the arguments
 		Object[] args = joinPoint.getArgs();
-		
+
 		Arrays.asList(args).stream().forEach(o -> logger.info("======>>>>>> argument: " + o));
 	}
-	
+
 	// add @AfterReturning advice
+	@AfterReturning(pointcut = "com.mdtsoft.aspect.AOPExpressions.forAppFlow()", returning = "result")
+	public void afterReturning(JoinPoint joinPoint, Object result) {
+		
+		// display method we are calling
+		String method = joinPoint.getSignature().toShortString();
+		logger.info("===>>> in @AfterReturning: calling method: " + method);
+		
+		// display data returned
+		logger.info("======>>> result: " + result);
+	}
 }
