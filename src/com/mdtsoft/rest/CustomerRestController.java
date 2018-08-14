a7b435e25470960e0b3d2130f048635ad9781c20
tutkuince <tutku.ince@outlook.com>
2018-08-14 13:33:42 +0300

    added annotations

--- src/com/mdtsoft/rest/CustomerRestController.java
+++ src/com/mdtsoft/rest/CustomerRestController.java
@@ -1,5 +1,10 @@
 package com.mdtsoft.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class CustomerRestController {
 
 }
