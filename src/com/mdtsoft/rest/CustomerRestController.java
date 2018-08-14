ea49af119c28b6f697ccb09204daac6c18a1776c
tutkuince <tutku.ince@outlook.com>
2018-08-14 15:08:58 +0300

    add mapping for POST /customers - add new customer

--- src/com/mdtsoft/rest/CustomerRestController.java
+++ src/com/mdtsoft/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,10 +31,20 @@ public class CustomerRestController {
 	// add mapping for GET /customers/{customerId}
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId) {
-		Customer customer = customerService.getById(customerId); 
-		
-		if(customer == null)
+		Customer customer = customerService.getById(customerId);
+
+		if (customer == null)
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+
+		return customer;
+	}
+
+	// add mapping for POST /customers - add new customer
+	@PostMapping("/customers")
+	public Customer saveCustomer(@RequestBody Customer customer) {
+		
+		customer.setId(0);
+		customerService.save(customer);
 		
 		return customer;
 	}
