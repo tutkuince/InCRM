f4617179492ddee60ee6539f4b92a24964a3722f
tutkuince <tutku.ince@outlook.com>
2018-08-14 14:05:45 +0300

    exception handler CustomerNotFoundException

--- src/com/mdtsoft/rest/error/CustomerRestExceptionHandler.java
+++ src/com/mdtsoft/rest/error/CustomerRestExceptionHandler.java
@@ -1,8 +1,25 @@
 package com.mdtsoft.rest.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice	// global exception handler
+@ControllerAdvice // global exception handler
 public class CustomerRestExceptionHandler {
-	
+
+	// Add an exception handler for CustomerNotFoundException
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException exc) {
+
+		// create CustomerErrorResponse
+		CustomerErrorResponse error = new CustomerErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+
+		// return ResponseEntity
+		return new ResponseEntity<CustomerErrorResponse>(error, HttpStatus.NOT_FOUND);
+	}
+
 }
