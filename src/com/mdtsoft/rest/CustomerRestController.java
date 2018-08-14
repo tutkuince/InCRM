a9f603e3d2807b39f802412a0bf564e4040fc881
tutkuince <tutku.ince@outlook.com>
2018-08-14 13:34:29 +0300

    autowire the CustomerService

--- src/com/mdtsoft/rest/CustomerRestController.java
+++ src/com/mdtsoft/rest/CustomerRestController.java
@@ -1,10 +1,17 @@
 package com.mdtsoft.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mdtsoft.service.CustomerService;
 
 @RestController
 @RequestMapping("/api")
 public class CustomerRestController {
 
+	// autowire the CustomerService
+	@Autowired
+	private CustomerService customerService;
+	
 }
