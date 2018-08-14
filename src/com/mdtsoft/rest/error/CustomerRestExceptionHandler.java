df5959ee28b402f66e446c8eae4662bee132fd91
tutkuince <tutku.ince@outlook.com>
2018-08-14 14:01:22 +0300

    global exception handler

---
+++ src/com/mdtsoft/rest/error/CustomerRestExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.mdtsoft.rest.error;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice	// global exception handler
+public class CustomerRestExceptionHandler {
+	
+}
