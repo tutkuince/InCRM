a1e47604e189a6d0c7ed064d841b7e7c33bb9696
tutkuince <tutku.ince@outlook.com>
2018-08-14 13:35:41 +0300

    add mapping for GET /customers

--- src/com/mdtsoft/rest/CustomerRestController.java
+++ src/com/mdtsoft/rest/CustomerRestController.java
@@ -1,9 +1,13 @@
 package com.mdtsoft.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mdtsoft.model.Customer;
 import com.mdtsoft.service.CustomerService;
 
 @RestController
@@ -13,5 +17,11 @@ public class CustomerRestController {
 	// autowire the CustomerService
 	@Autowired
 	private CustomerService customerService;
-	
+
+	// add mapping for GET /customers
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		return customerService.getAll();
+	}
+
 }
