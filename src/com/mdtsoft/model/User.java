659c0955a033fdc1fe4f57a2ee74b820b9d96101
tutkuince <tutku.ince@outlook.com>
2018-08-17 19:43:11 +0300

    User for Spring Security

---
+++ src/com/mdtsoft/model/User.java
@@ -0,0 +1,64 @@
+package com.mdtsoft.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@Column(name = "USERNAME", unique = true, nullable = false)
+	private String username;
+
+	@Column(name = "PASSWORD", nullable = false, columnDefinition = "char(68)")
+	private String password;
+
+	@Column(name = "ENABLED", nullable = false)
+	@Type(type = "org.hibernate.type.NumericBooleanType")
+	private boolean enabled;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+	private Set<Authorities> authorities = new HashSet<>();
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Set<Authorities> getAuthorities() {
+		return authorities;
+	}
+
+	public void setAuthorities(Set<Authorities> authorities) {
+		this.authorities = authorities;
+	}
+
+}
