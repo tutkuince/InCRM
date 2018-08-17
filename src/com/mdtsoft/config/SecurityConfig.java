1cf7d018d092a43d827bebdd4497d92aa6f4c287
tutkuince <tutku.ince@outlook.com>
2018-08-17 19:41:36 +0300

    added configure(HttpSecurity http)

--- src/com/mdtsoft/config/SecurityConfig.java
+++ src/com/mdtsoft/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.mdtsoft.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -18,5 +20,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(comboPooledDataSource);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/api/customers").hasRole("EMPLOYEE")
+			.antMatchers(HttpMethod.GET, "/api/customers/*").hasRole("EMPLOYEE")
+			.antMatchers(HttpMethod.POST, "/api/customers").hasAnyRole("MANAGER")
+			.antMatchers(HttpMethod.PUT, "/api/customers").hasAnyRole("MANAGER")
+			.antMatchers(HttpMethod.DELETE, "/api/customers/*").hasRole("ADMIN")
+			.and()
+			.formLogin()	
+			.loginPage("/showLoginPage")
+			.loginProcessingUrl("/authenticateTheUser")
+			.permitAll()
+			.and()
+			.logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/access-denied");
 	}
 }
