504cfe1d772c59884906d38b29098ca4769d3d9a
tutkuince <tutku.ince@outlook.com>
2018-08-17 19:42:52 +0300

    Authorities for Spring Security

---
+++ src/com/mdtsoft/model/Authorities.java
@@ -0,0 +1,49 @@
+package com.mdtsoft.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "AUTHORITIES")
+public class Authorities {
+
+	@Id
+	private Long id;
+
+	@Column(name = "AUTHORITY", nullable = false)
+	private String authority;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "USERNAME", nullable = false)
+	private User user;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
